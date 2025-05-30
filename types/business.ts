30041400881cc6ee8142545ee9ab43b61376d737
adamhalverson1 <76791907+adamhalverson1@users.noreply.github.com>
2025-05-29 21:57:58 -0500

    Update business.ts

--- types/business.ts
+++ types/business.ts
@@ -1,21 +1,30 @@
+export interface BusinessHours {
+  Sunday?: string;
+  Monday?: string;
+  Tuesday?: string;
+  Wednesday?: string;
+  Thursday?: string;
+  Friday?: string;
+  Saturday?: string;
+}
+
 export interface Business {
-    id: string;
-    name: string;
-    description: string;
-    categories: string;
-    category: string;
-    address: string;
-    image: string;
-    city: string;
-    state: string;
-    phone: string;
-    serviceArea: string;
-    email: string;
-    Facebook: string;
-    Instagram: string;
-    Twitter: string;
-    hours: string;
-    website: string;
-    attachments?: string[] | Attachment[];
-  }
-  
+  id: string;
+  name: string;
+  description: string;
+  categories: string;
+  category: string;
+  address: string;
+  image: string;
+  city: string;
+  state: string;
+  phone: string;
+  serviceArea: string;
+  email: string;
+  Facebook: string;
+  Instagram: string;
+  Twitter: string;
+  hours: BusinessHours; // 👈 changed from string to object
+  website: string;
+  attachments?: string[] | Attachment[];
+}
